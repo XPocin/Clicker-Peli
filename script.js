94cc5e99d72c60b3d041c4408253d465ff15c74f
XPocin <npaavilainen91@gmail.com>
2018-03-08 13:55:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    var pisteet = 0
+
+    function Nappi(){
+        var pisteet =+ 1;
+        console.log (pisteet);
+    }
+
+    function Show(){
+        var pisteet = 0;
+        document.getElementById("Show text").innerHTML = pisteet;
+    }
