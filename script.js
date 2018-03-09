2cecf9de5a83d0dfac7f8059235b0e60e98b2365
XPocin <npaavilainen91@gmail.com>
2018-03-09 13:19:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,18 @@
         console.log (pisteet);
     }
 
+
     function Show(){
         var pisteet = 0;
         document.getElementById("Show text").innerHTML = pisteet;
+    }
+
+    function kerroin1(){
+        var pisteet 
+    }
+    function kerroin2(){
+        var pisteet 
+    }
+    function kerroin3()){
+        var pisteet 
     }
