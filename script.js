cdce1e446b561a7ed86a06fe4fa821f4dfdaa8af
XPocin <npaavilainen91@gmail.com>
2018-03-14 14:00:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-    var pisteet = 0
+var pisteet = 0
+var kerroin = 1
+var laskuri = 0;
 
-    function Nappi(){
-        var pisteet =+ 1;
-        console.log (pisteet);
+function nappi(){
+        laskuri = laskuri + 1 * kerroin;
+        console.log('Pisteet: ' + laskuri);
+        tulosta();
     }
 
+function tulosta(){
+    console.log(laskuri);
+    document.getElementById("klikit").innerHTML = laskuri;
+}
+
+function laskuri(){
+    console.log(pisteet);
+    tulosta();
+}
 
     function Show(){
         var pisteet = 0;
         document.getElementById("Show text").innerHTML = pisteet;
     }
 
-    function kerroin1(){
-        var pisteet 
-    }
-    function kerroin2(){
-        var pisteet 
-    }
-    function kerroin3(){
-        var pisteet 
-    }
     function piirra(){
         var c=document.getElementById("Canvas");
         var ctx=c.getContext("2d");
